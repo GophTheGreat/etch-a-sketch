ad2ebb2fe5c42e21e2ccae171347b56d2bda398c
GophTheGreat <13459026+GophTheGreat@users.noreply.github.com>
2022-03-16 16:39:17 -1000

    create makeGrid

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 
 const buttons = document.querySelectorAll('button');
 
-let etchGrid = document.createElement('div');
+let etchGrid = [];
+
+const gridContainer = document.querySelector(".gridContainer");
+
+const makeGrid(){
+  for (let i = 0; i < 256; i++){
+    etchGrid[i] = document.createElement('div');
+    //etchGrid[i].style.backgroundColor = '#1DF9BD';
+    etchGrid[i].classList.add('cell');
+    //etchGrid[i].textContent = `-IIII-`;
+    etchGrid[i].textContent = `${i}`;
+    if (i%2 === 0){ 
+      etchGrid[i].style.backgroundColor = '#1DF9BD';
+      console.log("even!");
+    }
+    gridContainer.append(etchGrid[i]);
+  }
+}
+
+makeGrid();
