6dc68992630e148435fd954dc89ce31d5427791c
Goph <tealeverywhere@gmail.com>
2022-03-16 22:15:03 -1000

    implemented core bullshit

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 
-const buttons = document.querySelectorAll('button');
+const resetButton = document.querySelector('button');
+
+resetButton.addEventListener('click', reset)
+
+function reset(){
+  let etchGridAll = document.querySelectorAll('.dark')
+  etchGridAll.forEach((e) => e.classList.remove('dark'));
+}
 
 let etchGrid = [];
 
 const gridContainer = document.querySelector(".gridContainer");
 
-const makeGrid(){
-  for (let i = 0; i < 256; i++){
+function darken(e){
+  e.target.classList.add('dark');
+}
+
+function makeGrid(size){
+  for (let i = 0; i < size * size; i++){
     etchGrid[i] = document.createElement('div');
-    //etchGrid[i].style.backgroundColor = '#1DF9BD';
     etchGrid[i].classList.add('cell');
-    //etchGrid[i].textContent = `-IIII-`;
-    etchGrid[i].textContent = `${i}`;
-    if (i%2 === 0){ 
-      etchGrid[i].style.backgroundColor = '#1DF9BD';
-      console.log("even!");
-    }
+    etchGrid[i].addEventListener('mouseover', darken);
     gridContainer.append(etchGrid[i]);
+
   }
 }
 
-makeGrid();
+makeGrid(16);
